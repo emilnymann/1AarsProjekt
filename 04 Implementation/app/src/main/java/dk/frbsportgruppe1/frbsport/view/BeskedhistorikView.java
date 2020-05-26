f757faa292e46a90fc4505d53f164f980cabc5a3
Emil Nymann <emilnymann96@gmail.com>
2020-05-26 10:04:37 +0200

    Refactor Beskedhistorik til BeskedhistorikView for at undgå konflikter

--- 04 Implementation/app/src/main/java/dk/frbsportgruppe1/frbsport/view/BeskedhistorikView.java
+++ 04 Implementation/app/src/main/java/dk/frbsportgruppe1/frbsport/view/BeskedhistorikView.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import dk.frbsportgruppe1.frbsport.R;
 
-public class Beskedhistorik extends AppCompatActivity {
+public class BeskedhistorikView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
