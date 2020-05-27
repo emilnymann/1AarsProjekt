d1af6aa2c7582042b51032437d01817b3a34c6c8
ThunderGrove <rene699f@edu.easj.dk>
2020-05-27 11:30:44 +0200

    Tilføjet JavaDocs til Patientoversigt, PatientoversigtInterface, BehandlerErNullException og PatientErNullException

--- 04 Implementation/app/src/main/java/dk/frbsportgruppe1/frbsport/model/Patientoversigt.java
+++ 04 Implementation/app/src/main/java/dk/frbsportgruppe1/frbsport/model/Patientoversigt.java
@@ -6,9 +6,13 @@ import dk.frbsportgruppe1.frbsport.model.exceptions.BehandlerErNullException;
 import dk.frbsportgruppe1.frbsport.model.exceptions.PatientErNullException;
 
 public class Patientoversigt implements PatientoversigtInterface{
-    private Behandler behandler;//Kun en behandler kan være forbundet til en patientoversigt.
-    private ArrayList<Patient> patienter=new ArrayList<Patient>;//Flere patienter kan være forbundet til en patientoversigt.
+    private Behandler behandler;
+    private ArrayList<Patient> patienter=new ArrayList<Patient>;
 
+    /**
+    * @param b er den behandler patientoversigten er forbundet til.
+    * @throws BehandlerErNullException bliver udløst hvis denne method bilver kaldt unden en behandler.
+    */
     public void saetBehandler(Behandler b) throws BehandlerErNullException{
         if(b!=null){
             behandler=b;
@@ -17,6 +21,10 @@ public class Patientoversigt implements PatientoversigtInterface{
         }
     }
 
+    /**
+    * @param p er den patient som skal tilføjes til patientoversigten.
+    * @throws PatientErNullException bliver udløst hvis denne method bilver kaldt unden en patient.
+    */
     public void tilfoej(Patient p) throws PatientErNullException {
         if(p!=null){
             patienter.add(p);
@@ -25,10 +33,17 @@ public class Patientoversigt implements PatientoversigtInterface{
         }
     }
 
+    /**
+     * @param listId nummeret på en specefik patient. Nummeret bliver angivet ud fra hvornår patienten blev tilføjet patientoversigten.
+     * @return Returnere patient objektet på den ønskede patient.
+     */
     public Patient hentPatient(int listId){
         return patienter.get(listId);
     }
 
+    /**
+    * @return Retunere antal af patienter forbundet til patientoversigten.
+    */
     public int patientSize(){
         return patienter.size();
     }
--- 04 Implementation/app/src/main/java/dk/frbsportgruppe1/frbsport/model/exceptions/BehandlerErNullException.java
+++ 04 Implementation/app/src/main/java/dk/frbsportgruppe1/frbsport/model/exceptions/BehandlerErNullException.java
@@ -1,5 +1,9 @@
 package dk.frbsportgruppe1.frbsport.model.exceptions;
 
+/**
+ * Bruges til give fejl, hvis behandler ikke er sat.
+ * Udvider Exception for test kan gribe fejlen.
+ */
 public class BehandlerErNullException extends Exception{
     public BehandlerErNullException(String fejlBesked){
         super(fejlBesked);
