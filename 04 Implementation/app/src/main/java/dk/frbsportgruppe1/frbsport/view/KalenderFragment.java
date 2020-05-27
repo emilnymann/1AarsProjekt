656bee3e1b19b34adb301c89f57ae0d74bfc42d7
Emil Nymann <emilnymann96@gmail.com>
2020-05-27 10:06:47 +0200

    Tilføjet BottomNavigationView funktionalitet
    * Skift activities ud med fragments
    * Tilføjet funktionalitet til at skifte mellem fragments med BottomNavigationView med event handler
    * Angiv baggrundsfarve på fragment layouts så de ikke vises over hinanden
    * Refactor BeskedhistorikActivity til PatientMainActivity

---
+++ 04 Implementation/app/src/main/java/dk/frbsportgruppe1/frbsport/view/KalenderFragment.java
@@ -0,0 +1,27 @@
+package dk.frbsportgruppe1.frbsport.view;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import dk.frbsportgruppe1.frbsport.R;
+
+public class KalenderFragment extends Fragment {
+
+
+    public KalenderFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_kalender, container, false);
+    }
+
+}
