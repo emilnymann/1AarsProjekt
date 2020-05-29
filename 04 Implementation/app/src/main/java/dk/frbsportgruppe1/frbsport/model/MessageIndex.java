98f9be2a3e6219b470e9695b7a8defba75bba8a2
Emil Nymann <emilnymann96@gmail.com>
2020-05-29 14:08:16 +0200

    MessageIndex.sendMessage thrower DateIsNullException

--- 04 Implementation/app/src/main/java/dk/frbsportgruppe1/frbsport/model/MessageIndex.java
+++ 04 Implementation/app/src/main/java/dk/frbsportgruppe1/frbsport/model/MessageIndex.java
@@ -1,5 +1,6 @@
 package dk.frbsportgruppe1.frbsport.model;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Observable;
 
@@ -51,15 +52,19 @@ public class MessageIndex extends Observable implements MessageIndexInterface {
     }
 
     @Override
-    public void sendMessage(String message, User sender) throws MessageTooLongException, MessageIsNullException, InvalidMessageException, SenderIsNullException {
-        if (message.length() > 255) {
+    public void sendMessage(String messageText, User sender) throws MessageTooLongException, MessageIsNullException, InvalidMessageException, SenderIsNullException, DateIsNullException {
+        if (messageText.length() > 255) {
             throw new MessageTooLongException("Du kan ikke lave en besked på mere end 255 tegn");
-        } else if (message.isEmpty()) {
+        } else if (messageText.isEmpty()) {
             throw new MessageIsNullException("Beskeden kan ikke sendes");
-        } else if (message.matches("^\\s*$")){
+        } else if (messageText.matches("^\\s*$")){
             throw new InvalidMessageException("Beskeden kan ikke sendes");
         } else if (sender == null) {
             throw new SenderIsNullException("Beskeden kan ikke sendes uden afsender");
+        } else {
+            Message message = new Message(messageText, sender);
+            message.setDateTime(LocalDateTime.now());
+            this.addMessage(message);
         }
     }
 
@@ -68,7 +73,7 @@ public class MessageIndex extends Observable implements MessageIndexInterface {
     }
 
     public ArrayList<Message> getMessages() {
-        messages.sort(new SortMessages());
+//        messages.sort(new SortMessages());
         return messages;
     }
 
