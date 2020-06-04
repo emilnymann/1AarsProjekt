8fdb5f6bf6af5d57c4371d133f8d3b36a4b2c985
Emil Nymann Sølyst <emilnymann96@gmail.com>
2020-06-04 15:51:06 +0200

    Rettet stavefejl i PatientMainActivityPagerAdapter

--- 04 Implementation/app/src/main/java/dk/frbsportgruppe1/frbsport/view/PatientMainViewPagerAdapter.java
+++ 04 Implementation/app/src/main/java/dk/frbsportgruppe1/frbsport/view/PatientMainViewPagerAdapter.java
@@ -5,9 +5,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-public class PaitentMainViewPagerAdapter extends FragmentStateAdapter {
+public class PatientMainViewPagerAdapter extends FragmentStateAdapter {
 
-    public PaitentMainViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+    public PatientMainViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
         super(fragmentActivity);
     }
 
